fe596db26ea1c4e3147dedf73461e404bac2416f
mohanad oweidat <mohanad.oweidat@hotmail.com>
2019-01-21 20:21:19 +0100

    I fixed the webservice and connected the database with the app!

---
+++ WebApplication1/Controllers/AccountsController.cs
@@ -0,0 +1,72 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class AccountsController : ApiController
+    {
+
+        //Get Accounts
+        public IEnumerable<Accounts> Get()
+        {
+            using (MSSQL400028Entities1 entities = new MSSQL400028Entities1())
+            {
+                return entities.Accounts.ToList();
+            }
+        }
+
+        //Get Accounts by Id
+        public Accounts Get(int id)
+        {
+            using (MSSQL400028Entities1 entities = new MSSQL400028Entities1())
+            {
+                return entities.Accounts.FirstOrDefault(e => e.ID == id);
+            }
+        }
+
+
+        //Post Accounts
+
+        public void Post([FromBody]Accounts accounts)
+        {
+            using (MSSQL400028Entities1 entities = new MSSQL400028Entities1())
+            {
+                  entities.Accounts.Add(accounts);
+                entities.SaveChanges();
+            }
+        }
+
+
+        //Delete Accounts
+        public void Delete (int id)
+        {
+            using (MSSQL400028Entities1 entities = new MSSQL400028Entities1())
+            {
+                entities.Accounts.Remove(entities.Accounts.FirstOrDefault(e => e.ID == id));
+                entities.SaveChanges();
+            }
+        } 
+
+
+        //Update Accounts (Put)
+        public void Put(int id, [FromBody] Accounts accounts)
+        {
+            using (MSSQL400028Entities1 entities = new MSSQL400028Entities1())
+            {
+               var entity =  entities.Accounts.FirstOrDefault(e => e.ID == id);
+
+                entity.Username = accounts.Username;
+                entity.Password = accounts.Password;
+                entity.Class = accounts.Class;
+                entity.isAdmin = accounts.isAdmin;
+                entities.SaveChanges();
+            }
+        }
+
+    }
+}
---
+++ WebApplication1/Controllers/InfoController.cs
@@ -0,0 +1,68 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class InfoController : ApiController
+    {
+        //Get Info
+        public IEnumerable<Info> Get()
+        {
+            using (MSSQL400028Entities1 entities = new MSSQL400028Entities1())
+            {
+                return entities.Info.ToList();
+            }
+        }
+
+        //Get info by id
+        public Info Get(int id)
+        {
+            using (MSSQL400028Entities1 entities = new MSSQL400028Entities1())
+            {
+                return entities.Info.FirstOrDefault(e => e.ID == id);
+            }
+        }
+
+        //Post Info
+          public void Post([FromBody]Info info)
+        {
+            using (MSSQL400028Entities1 entities = new MSSQL400028Entities1())
+            {
+                entities.Info.Add(info);
+                entities.SaveChanges();
+            }
+        }
+
+        //Delete Info
+        public void Delete(int id)
+        {
+            using (MSSQL400028Entities1 entities = new MSSQL400028Entities1())
+            {
+                entities.Info.Remove(entities.Info.FirstOrDefault(e => e.ID == id));
+                entities.SaveChanges();
+            }
+        }
+
+
+        //Update Info (Put)
+        public void Put(int id, [FromBody] Info info)
+        {
+            using (MSSQL400028Entities1 entities = new MSSQL400028Entities1())
+            {
+                var entity = entities.Info.FirstOrDefault(e => e.ID == id);
+
+                entity.Studentname = info.Studentname;
+                entity.ATime = info.ATime;
+                entity.Date = info.Date;
+                entities.SaveChanges();
+            }
+        }
+
+
+    }
+}
